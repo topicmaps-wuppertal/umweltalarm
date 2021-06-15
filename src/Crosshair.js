309531e76a29c785a4e4e2293b330b18580ec958
helllth <th@cismet.de>
2021-06-15 11:56:14 +0200

    improve structure

---
+++ src/Crosshair.js
@@ -0,0 +1,49 @@
+import { useContext } from "react";
+
+import { ResponsiveTopicMapContext } from "react-cismap/contexts/ResponsiveTopicMapContextProvider";
+
+const Comp = () => {
+  const { windowSize } = useContext(ResponsiveTopicMapContext);
+
+  return (
+    <div>
+      <div
+        style={{
+          position: "absolute",
+          top: windowSize?.height / 2 - 1,
+          width: windowSize?.width || "100%",
+          height: "2px",
+          backgroundColor: "#00000023",
+          zIndex: "100000",
+          pointerEvents: "none",
+        }}
+      ></div>
+      <div
+        style={{
+          position: "absolute",
+          left: windowSize?.width / 2 - 1,
+          height: windowSize?.height || "100%",
+          width: "2px",
+          backgroundColor: "#00000023",
+          zIndex: "100000",
+          pointerEvents: "none",
+        }}
+      ></div>
+      <div
+        style={{
+          borderRadius: "50%",
+          position: "absolute",
+          left: windowSize?.width / 2 - 10,
+          top: windowSize?.height / 2 - 10,
+          height: "20px",
+          width: "20px",
+          backgroundColor: "#00000023",
+          zIndex: "100000",
+          pointerEvents: "none",
+        }}
+      ></div>
+    </div>
+  );
+};
+
+export default Comp;
--- src/UmweltalarmMap.js
+++ src/UmweltalarmMap.js
@@ -9,12 +9,12 @@ import "react-cismap/topicMaps.css";
 import { md5FetchText, fetchJSON, md5FetchJSON } from "react-cismap/tools/fetching";
 import { getGazDataForTopicIds } from "react-cismap/tools/gazetteerHelper";
 
-import { ResponsiveTopicMapContext } from "react-cismap/contexts/ResponsiveTopicMapContextProvider";
 import { getClusterIconCreatorFunction } from "react-cismap/tools/uiHelper";
 import TopicMapComponent from "react-cismap/topicmaps/TopicMapComponent";
 import FeatureCollection from "react-cismap/FeatureCollection";
 import GenericInfoBoxFromFeature from "react-cismap/topicmaps/GenericInfoBoxFromFeature";
 import getGTMFeatureStyler from "react-cismap/topicmaps/generic/GTMStyler";
+import Crosshair from "./Crosshair";
 
 const host = "https://wupp-topicmaps-data.cismet.de";
 
@@ -43,8 +43,6 @@ const getData = async (setGazData, setInfoData) => {
 };
 
 function UmweltalarmMap() {
-  const { windowSize } = useContext(ResponsiveTopicMapContext);
-
   const [gazData, setGazData] = useState([]);
   const [infoData, setInfoData] = useState([]);
   useEffect(() => {
@@ -52,41 +50,7 @@ function UmweltalarmMap() {
   }, []);
   return (
     <div>
-      <div
-        style={{
-          position: "absolute",
-          top: windowSize?.height / 2 - 1,
-          width: windowSize?.width || "100%",
-          height: "2px",
-          backgroundColor: "#00000023",
-          zIndex: "100000",
-          pointerEvents: "none",
-        }}
-      ></div>
-      <div
-        style={{
-          position: "absolute",
-          left: windowSize?.width / 2 - 1,
-          height: windowSize?.height || "100%",
-          width: "2px",
-          backgroundColor: "#00000023",
-          zIndex: "100000",
-          pointerEvents: "none",
-        }}
-      ></div>
-      <div
-        style={{
-          borderRadius: "50%",
-          position: "absolute",
-          left: windowSize?.width / 2 - 10,
-          top: windowSize?.height / 2 - 10,
-          height: "20px",
-          width: "20px",
-          backgroundColor: "#00000023",
-          zIndex: "100000",
-          pointerEvents: "none",
-        }}
-      ></div>
+      <Crosshair />
       <TopicMapComponent gazData={gazData}></TopicMapComponent>
     </div>
   );
